3c9cddf4576fcabecbe30df5abc05a9ae12dd19b
Seunga Lee <leeseunga1590@gmail.com>
2024-05-25 13:01:01 +0900

    디자인 수정 1

--- app/src/main/java/com/bokchi/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/auth/IntroActivity.kt
@@ -36,24 +36,5 @@ class IntroActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        binding.noAccountBtn.setOnClickListener {
-            auth.signInAnonymously()
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-
-                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
-
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(intent)
-
-                    } else {
-
-                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
-
-                    }
-                }
-        }
-
     }
 }
